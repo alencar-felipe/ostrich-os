6dc04ab081c83bd7b0b3bcfa31e0b5dafce6c33c
alencar-felipe <alencar.felipe.paiva@gmail.com>
2022-07-13 23:43:35 -0300

    kernel "process" working

--- source/scheduler.c
+++ source/scheduler.c
@@ -5,17 +5,6 @@ enum os_task_status {
 	OS_TASK_STATUS_ACTIVE,
 };
 
-struct os_task {
-	/* The stack pointer (sp) has to be the first element as it is located
-	   at the same address as the structure itself (which makes it possible
-	   to locate it safely from assembly implementation of PendSV_Handler).
-	   The compiler might add padding between other structure elements. */
-	volatile uint32_t sp;
-	void (*handler)(void *params);
-	void *params;
-	volatile enum os_task_status status;
-};
-
 static enum state {
 	STATE_DEFAULT = 1,
 	STATE_INITIALIZED,
@@ -23,11 +12,8 @@ static enum state {
 	STATE_STARTED,
 } state = STATE_DEFAULT;
 
-static struct task_table {
-	struct os_task tasks[SCHEDULER_MAX];
-	volatile uint32_t current_task;
-	uint32_t size;
-} task_table;
+static process_t processes[SCHEDULER_MAX];
+static uint32_t current;
 
 volatile uint32_t *os_curr_task;
 volatile uint32_t *os_next_task;
@@ -46,9 +32,12 @@ uint8_t scheduler_init(void)
 	if (state != STATE_DEFAULT)
 		return 0;
 
-	for(uint32_t i = 0; i < sizeof(task_table); i++) {
-		((uint8_t *) &task_table)[i] = 0;
+	for(uint32_t i = 0; i < SCHEDULER_MAX; i++) {
+		processes[i].state = NONE;
 	}
+
+	processes[0].state = READY;
+	current = 0;
 
 	state = STATE_INITIALIZED;
 
@@ -64,21 +53,22 @@ uint8_t scheduler_create(
 	if (state != STATE_INITIALIZED && state != STATE_TASKS_INITIALIZED) {
 		return -1;
 	}
-		
-	if (task_table.size >= SCHEDULER_MAX) return -1;
+
+	uint32_t i = 0;
+	for(; i < SCHEDULER_MAX; i++) if(processes[i].state == NONE) break;
+	if(i == SCHEDULER_MAX) return -1;
+
 	if ((stack_size % sizeof(uint32_t)) != 0) return -1;
 
 	uint32_t stack_offset = (stack_size/sizeof(uint32_t));
 
 	// Initialize the task structure and set SP to the top of the stack
 	//   minus 16 words (64 bytes) to leave space for storing 16 registers:
-	struct os_task *task = &task_table.tasks[task_table.size];
-	task->handler = handler;
-	task->params = params;
-	task->sp = (uint32_t)(stack+stack_offset-16);
-	task->status = OS_TASK_STATUS_IDLE;
 
-	registers_t *registers = (registers_t *) (task->sp);
+	processes[i].sp = (uint32_t) (stack+stack_offset-16);
+	processes[i].state = READY;
+
+	registers_t *registers = (registers_t *) (processes[i].sp);
 
 	// Save init. values of registers which will be restored on exc. return:
 	// - XPSR: Default value (0x01000000)
@@ -92,7 +82,6 @@ uint8_t scheduler_create(
 	registers->r0 = (uint32_t) params;
 
 	state = STATE_TASKS_INITIALIZED;
-	task_table.size++;
 
 	return 1;
 }
@@ -109,8 +98,8 @@ uint8_t scheduler_start(uint32_t systick_ticks)
 	if (ret_val != 0) return -1;
 
 	// start the first task
-	os_next_task = &task_table.tasks[task_table.current_task].sp;
-	os_curr_task = &task_table.tasks[task_table.current_task].sp;
+	os_next_task = &(processes[current].sp);
+	os_curr_task = &(processes[current].sp);
 	state = STATE_STARTED;
 
 	//__set_MSP(os_curr_task->sp+64); // set PSP to the top of task's stack
@@ -128,12 +117,12 @@ void systick_handler(void)
 	os_curr_task = os_next_task; //&task_table.tasks[task_table.current_task].sp;
 	//os_curr_task->status = OS_TASK_STATUS_IDLE;
 
-	// select next task
-	task_table.current_task++;
-	if (task_table.current_task >= task_table.size)
-		task_table.current_task = 0;
+	while(1) {
+		if(++current > SCHEDULER_MAX) current = 0;
+		if(processes[current].state == READY) break;
+	}
 
-	os_next_task = &task_table.tasks[task_table.current_task].sp;
+	os_next_task = &(processes[current].sp);
 	//os_next_task->status = OS_TASK_STATUS_ACTIVE;
 
 	// trigger PendSV which performs the actual context switch
